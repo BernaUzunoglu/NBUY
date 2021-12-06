042a6e175118ae7c788a34fa867b77c0a481c00e
Berna UZUNOĞLU <68403247+BernaUzunoglu@users.noreply.github.com>
2021-12-06 11:34:24 +0300

    codefirst after table

---
+++ DataAcecess/KuzeyCodeFirst/Migrations/20211206082241_Emre3.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KuzeyCodeFirst.Migrations
+{
+    public partial class Emre3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Aciklama",
+                table: "Kategoriler",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Aciklama",
+                table: "Kategoriler",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
---
+++ DataAcecess/KuzeyCodeFirst/Models/Urun.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KuzeyCodeFirst.Models
+{
+    [Table("Urunler")]
+    public class Urun
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required,StringLength(50)]
+        public string Ad { get; set; }
+        public decimal Fiyat { get; set; }
+        public int KategorId { get; set; }//nullable olursa kategorisiz ürün eklenebilir.
+        [Range(0,1000)]
+        public int StokMiktari { get; set; }
+
+
+        [ForeignKey(nameof(KategorId))]
+        public Kategori Kategori { get; set; }
+    }
+}
